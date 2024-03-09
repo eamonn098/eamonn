b3c869cdc0f02598e3b0d073fe9e59131adb9e8d
eamonn098 <162358586+eamonn098@users.noreply.github.com>
2024-03-09 12:55:14 +0000

    Update originalcode.py

--- originalcode.py
+++ originalcode.py
@@ -33,7 +33,7 @@ try:
 
     logging.info("read bmp file on window")
     Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+    bmp = Image.open(os.path.join(picdir, 'tester.bmp'))
     Himage2.paste(bmp, (50,10))
     epd.display(epd.getbuffer(Himage2))
     time.sleep(2)
