32923526f5e803700f02393c9001d2f20da5f0c8
eamonn098 <162358586+eamonn098@users.noreply.github.com>
2024-03-09 12:07:06 +0000

    test 2

--- imagetobwtest.py
+++ imagetobwtest.py
@@ -1,6 +1,21 @@
-from PIL import Image, ImageEnhance
-im = Image.open("CAT-logo.jpg")
-print(im.format, im.size, im.mode)
-im = im.convert("L")
+from PIL import Image
 
-im.show()
+
+#set canvas size of eink
+Canv_size= (800,460)
+#blank canvas for screen
+Canv= Image.new("L",Canv_size, color=255)
+#open chonsen image
+im = Image.open("ALBUMIMG2.png")
+#get image info
+print(im.format, im.size, im.mode)
+#image to BW
+im = im.convert("L")
+#sq fit to scren
+im = im.resize((460,460))
+#overlay
+Canv.paste(im, (0,0)) 
+Canv.show()
+print(im.format, im.size, im.mode)
+Canv.save("tester.bmp")
+
