4b5d1e8f51ac282fbb311d20d6578ea4fc56b530
eamonn098 <162358586+eamonn098@users.noreply.github.com>
2024-03-09 19:39:11 +0000

    Update art_test_2 copy.py

--- art_test_2 copy.py
+++ art_test_2 copy.py
@@ -26,7 +26,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 results = sp.currently_playing()
 url=results['item']['album']['images'][0]['url']
 name=results['item']['album']['name']
-print(name)
 
 #set canvas size of eink
 Canv_size= (800,480)
@@ -35,14 +34,12 @@ Canv= Image.new("L",Canv_size, color=255)
 #open chonsen image from spotify url
 im = Image.open(requests.get(url, stream=True).raw)
 #get image info
-print(im.format, im.size, im.mode)
 #image to BW
 im = im.convert("P")
 #sq fit to scren
 im = im.resize((460,460))
 #overlay
 Canv.paste(im, (160,10))
-Canv.show()
 print(im.format, im.size, im.mode)
 Canv.save(os.path.join(picdir, 'tester.bmp'))
 
